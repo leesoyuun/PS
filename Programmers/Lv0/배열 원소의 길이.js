783ab933ff67ced3a6abf69184d695cdf739869c
leesoyuun <leesoyun4206@naver.com>
2022-11-27 21:44:56 +0900

    배열 원소의 길이 other ver

--- Programmers/Lv0/배열 원소의 길이.js
+++ Programmers/Lv0/배열 원소의 길이.js
@@ -1,8 +1,13 @@
-function solution(strlist) {
-    var answer = [];
-    for(let i = 0 ; i < strlist.length; i++){
-        answer.push(strlist[i].split('').length);
-    }
-    return answer;
+// function solution(strlist) {
+//     var answer = [];
+//     for(let i = 0 ; i < strlist.length; i++){
+//         answer.push(strlist[i].split('').length);
+//     }
+//     return answer;
+// }
+
+function solution(strlist){
+    return strlist.map((el)=>el.length);
 }
+// 사실상 map쓰라고 준 문제
 solution(["We", "are", "the", "world!"])
