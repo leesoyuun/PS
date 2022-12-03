eb1a7891c6d34a6b43fc431c45b141ada01d578d
leesoyuun <leesoyun4206@naver.com>
2022-12-03 22:20:11 +0900

    other solve : 문자열안에 문자열

--- Programmers/Lv0/문자열안에 문자열.js
+++ Programmers/Lv0/문자열안에 문자열.js
@@ -1,8 +1,12 @@
+// function solution(str1, str2) {
+//     var answer = 0;
+//     if(str1.includes(str2) == true)
+//         answer = 1;
+//     else
+//         answer = 2;
+//     return answer;
+// }
+
 function solution(str1, str2) {
-    var answer = 0;
-    if(str1.includes(str2) == true)
-        answer = 1;
-    else
-        answer = 2;
-    return answer;
+    return str1.split(str2).length > 1 ? 1: 2;
 }
