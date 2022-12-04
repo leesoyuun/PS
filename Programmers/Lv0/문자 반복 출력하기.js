e8f54a3f099279aae6c97bf8b8d4df08a31be202
leesoyuun <leesoyun4206@naver.com>
2022-12-04 22:48:15 +0900

    Other solve : 문자 반복 출력하기

--- Programmers/Lv0/문자 반복 출력하기.js
+++ Programmers/Lv0/문자 반복 출력하기.js
@@ -1,8 +1,12 @@
+// function solution(my_string, n) {
+//     const string = my_string.split('');
+//     let answer = [];
+//     for(let i = 0 ; i < string.length; i++){
+//         answer.push(string[i].repeat(n));
+//     }
+//     return answer.join('');
+// }
 function solution(my_string, n) {
-    const string = my_string.split('');
-    let answer = [];
-    for(let i = 0 ; i < string.length; i++){
-        answer.push(string[i].repeat(n));
-    }
-    return answer.join('');
+    var answer = [...my_string].map((v) => v.repeat(n)).join("")
+    return answer;
 }
