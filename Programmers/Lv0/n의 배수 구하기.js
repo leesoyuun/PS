96bf80d0352ce447bc342c66ffb857dad609c2ff
leesoyuun <leesoyun4206@naver.com>
2022-12-15 22:45:35 +0900

    Other solve: n의 배수 구하기

--- Programmers/Lv0/n의 배수 구하기.js
+++ Programmers/Lv0/n의 배수 구하기.js
@@ -1,3 +1,12 @@
-function solution(n, numlist) {
+/*function solution(n, numlist) {
     return numlist.filter((num)=> num % n === 0 )
+}*/
+
+function solution(n, numlist) {
+    var answer = [];
+    numlist.map((k)=> {
+        if(k%n === 0)
+            answer.push(k);
+    });
+    return answer;
 }
