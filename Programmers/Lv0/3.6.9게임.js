d22e2e590239af4937f405438cafbf9564f19564
leesoyuun <leesoyun4206@naver.com>
2022-12-25 21:36:56 +0900

    369게임

---
+++ Programmers/Lv0/3.6.9게임.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    var answer = 0;
+    var test = []
+    var order_num = [...String(order)];
+    for(let i of order_num){
+        if(i.includes('3') || i.includes('6') || i.includes('9')){
+            answer++;
+        }
+    }
+    return answer;
+
+}
