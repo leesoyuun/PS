ad686b8e40c06a0f00f9557a0648b5a619a70ed5
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-02-06 21:43:28 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i of my_string){
+        if(i == i.toUpperCase()){
+            answer+=i.toLowerCase()
+        }else{
+            answer+=i.toUpperCase()
+        }
+    }
+    return answer;
+ 
+}
