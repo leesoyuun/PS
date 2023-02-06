8e7369653d145494885f602837adafd406f6018d
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-02-06 21:39:48 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [];
+    var odd = 0;
+    var even = 0;
+    for(let i of num_list){
+        i%2 == 0 ? even++ : odd++;
+    }
+    answer.push(even,odd)
+    return answer;
+}
