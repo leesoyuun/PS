508af582fee0e1ef06b965aed7a2ff00ec3b203a
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-02-06 21:39:30 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -0,0 +1,9 @@
+function solution(n, k) {
+    var answer = 0;
+    let free_drink = 0;
+    if(n > 9){
+        free_drink = Math.floor(n/10);
+    }
+    answer = n*12000 + (k-free_drink) * 2000
+    return answer;
+}
