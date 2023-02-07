9e27c0dbfac91730c3f888c7ff44d86ccc40df30
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-02-07 22:57:55 +0900

    [level 0] Title: 모스부호 (1), Time: 4.49 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,23 @@
+function solution(letter) {
+    let answer = [];
+    var morse = {
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'}
+            
+    let one_letter = letter.split(" ");
+    let morse_index = Object.keys(morse);
+    console.log(one_letter)
+    console.log(Object.keys(morse))
+    for(let i = 0 ; i<one_letter.length; i++){
+        for(let j = 0 ; j <morse_index.length;j++){
+            if(morse_index[j] === one_letter[i]){
+                answer.push(Object.values(morse)[j])
+            }
+        }
+    }
+
+    return answer.join('')
+}
