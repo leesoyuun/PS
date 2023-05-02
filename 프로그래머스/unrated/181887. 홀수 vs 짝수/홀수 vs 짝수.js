6461a015303538aabbffc004b46a8d101fdd4226
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-05-02 21:47:51 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    for(let i = 0; i <num_list.length;i+=2)
+        odd+= num_list[i];
+    for(let i = 1; i <num_list.length; i+=2)
+        even+=num_list[i];
+    return Math.max(odd,even);
+}
