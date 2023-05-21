83b07e67035f25eb3aaa9853a62c7eda3c0a9556
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-05-21 16:03:38 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+    var answer = 0;
+    if((a == b) && (a == c) && (b == c))
+        answer = (a + b + c)*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3);
+    else
+        answer = (a + b + c)*(a**2 + b**2 + c**2);
+    if((a != b) && (a != c) && (b !=c))
+        answer = a + b + c;
+    return answer;
+}
