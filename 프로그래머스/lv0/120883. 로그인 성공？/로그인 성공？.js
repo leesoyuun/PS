c26d4b9be56612c2365cb906a941bf11fc91a84b
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-05-24 17:48:41 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,9 @@
+function solution(id_pw, db) {
+    for(let i = 0 ; i < db.length ; i++){
+        if(id_pw[0] === db[i][0] && id_pw[1] === db[i][1])
+            return 'login';
+        if(id_pw[0] === db[i][0] && id_pw[1] != db[i][1])
+            return 'wrong pw';
+    }
+    return 'fail';
+}
