4cec5e7121c08de8becd627207d6f86d4b7a96fb
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2023-11-29 21:41:10 +0900

    Update prob3.js

--- Inflearn/section3/prob3.js
+++ Inflearn/section3/prob3.js
@@ -4,7 +4,7 @@ function solution(str) {
   for(let i of str){
     if(!isNaN(i)) answer += i;
   }
-  return answer;
+  return parseInt(answer);
 }
 let str = "g0en2T0s8eSoft";
 console.log(solution(str));
