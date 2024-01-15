0e77d2e97940f49f38183036c1cc6a1e0cd88b53
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2024-01-15 22:46:05 +0900

    [level Lv.1] Title: 완주하지 못한 선수, Time: 28.02 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/Lv.1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,25 @@
-function solution(participant, completion) {
-    let participant_sort = participant.sort();
-    let completion_sort = completion.sort();
+// function solution(participant, completion) {
+//     let participant_sort = participant.sort();
+//     let completion_sort = completion.sort();
 
-    for(let i = 0 ; i < participant_sort.length ; i++){
-        if(participant_sort[i] !== completion_sort[i]) return participant_sort[i];
+//     for(let i = 0 ; i < participant_sort.length ; i++){
+//         if(participant_sort[i] !== completion_sort[i]) return participant_sort[i];
+//     }
+// }
+function solution(participant, completion) {
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
 }
