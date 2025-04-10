1649f2f08180c4f851396df4cca8d6208a2d420b
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-10 20:43:55 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,3 @@
 function solution(x) {
-    let answer = 0;
-    let sum = x.toString().split('').reduce((acc,cur)=>acc+=Number(cur),0);
-    return x%sum ? false : true;
+    return x % x.toString().split('').map((val) => +val).reduce((acc,cur) => acc+= cur) === 0
 }
