6707e149c870fa9a65bedd8083dae0997566ef25
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-13 15:45:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,3 @@
 function solution(numbers) {
-    let onetonine = [0,1,2,3,4,5,6,7,8,9];
-    let answer = [];
-    for(let i of onetonine){
-        if(!numbers.includes(i)) answer.push(i);
-    }
-    return answer.reduce((acc,cur)=>acc+=cur);
+    return 45 - numbers.reduce((acc,cur) => acc + cur);
 }
