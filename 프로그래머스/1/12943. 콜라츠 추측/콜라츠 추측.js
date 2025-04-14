d0dfdd11346dee2863a220e43a2a9661fed68040
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-14 21:27:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,12 @@
 function solution(num) {
-    let cnt = 0;
-    if(num === 1) return 0;
-    while(true){
-        if(num%2==0){
-            num = num/2;
-            cnt++;
-        }else{
-            num = num*3 + 1;
-            cnt++;
-        }
-        if(num === 1) break;
+    var cnt = 0;
+    var numTemp = num;
+    
+    while(cnt <= 500){
+        if(numTemp === 1) break;
+        if(numTemp % 2 === 0)numTemp = numTemp/2;
+        else numTemp = numTemp * 3 + 1;
+        cnt++;
     }
-    if(cnt >= 500) return -1;
-    else return cnt;
+    return cnt >= 500 ? -1 : cnt;
 }
