68c4145030df915b83b154db2862cffbfaa7558c
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-18 23:16:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.11 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,12 +1,16 @@
+function measure(number){
+    var measureNum = 0;
+    for(let i = 1 ; i <= number; i++){
+        if(number % i === 0) measureNum++;
+    }
+    return measureNum;
+}
+
 function solution(left, right) {
-    let answer = 0;
-    for(let i = left ; i <= right ; i++){
-        let cnt = 0;
-        for(let j = 1 ; j <= i ; j++){
-            if(i%j===0) cnt++;
-        }
-        if(cnt%2===0) answer+=i;
-        else answer -=i;
+    var answer = 0;
+    for(let i = left; i <= right; i++){
+        if(measure(i) % 2 === 0) answer+= i;
+        else answer-= i;
     }
     return answer;
 }
