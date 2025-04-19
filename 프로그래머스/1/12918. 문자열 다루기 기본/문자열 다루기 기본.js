ec77c38daddf2315a743ae292ee40cc3f3bd0782
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-19 14:54:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,12 +1,9 @@
 function solution(s) {
-    let answer = true;
+    var answer = true;
     if(s.length === 4 || s.length === 6){
         for(let i = 0 ; i < s.length ; i++){
             if(isNaN(s[i])) answer = false;
         }
-    }else{
-        answer = false;
-    }
-    
+    }else answer = false;
     return answer;
 }
