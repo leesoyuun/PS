813679cb0278cb471b8ea51abfaa2d2167d5b090
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-19 14:59:26 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.66 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,11 @@
 function solution(arr1, arr2) {
     var answer = [];
-    for(let i = 0 ; i < arr1.length ; i++){
-        let temp = [];
-        for(let j = 0 ; j < arr1[i].length; j++){
-            temp.push(arr1[i][j] + arr2[i][j]);
+    for(let i = 0; i <arr1.length; i++){
+        var tempArr = [];
+        for(let j = 0; j < arr1[i].length ; j++){
+            tempArr.push(arr1[i][j] + arr2[i][j]);
         }
-        answer.push(temp);
+        answer.push(tempArr);
     }
-    return answer;
+    return answer
 }
