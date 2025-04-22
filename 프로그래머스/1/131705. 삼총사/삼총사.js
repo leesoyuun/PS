1f85116af9404c272e2dbd31ac1b3609db1eca77
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-22 22:16:56 +0900

    [level 1] Title: 삼총사, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,10 +1,9 @@
 function solution(number) {
-    let answer = 0;
-    let number_len = number.length
-    for(let i = 0 ; i < number_len ; i++){
-        for(let j = i+1; j < number_len ; j++){
-            for(let k = j+1 ; k < number_len ; k++){
-                number[i]+number[j]+number[k] == 0 ? answer++ : 0;
+    var answer = 0;
+    for(let i = 0 ; i < number.length; i++){
+        for(let j = i+1; j < number.length; j++){
+            for(let k = j+1 ; k < number.length; k++){
+                if(number[i] + number[j] + number[k] === 0) answer++;
             }
         }
     }
