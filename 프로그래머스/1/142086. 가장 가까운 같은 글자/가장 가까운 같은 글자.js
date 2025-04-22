98f0de1cbbe266a4a38f0dcc74ce0c584cbff9fa
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-22 22:36:24 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 8.64 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,8 @@
 function solution(s) {
-    let answer = [-1];
+    var answer = [-1];
     for(let i = 1 ; i < s.length ; i++){
-        let temp = [];
-        for(let j = 0 ; j < i; j++){
-            if(s[i] === s[j]) {
-                temp.push(i-j)
-            }
-        }
-        if(temp.length === 0 ) answer.push(-1);
-        else if(temp.length === 1) answer.push(temp[0]);
-        else answer.push(Math.min(...temp));
-        
+        const sliceS = s.slice(0,i);
+        answer.push(sliceS.lastIndexOf(s[i]) === -1 ? -1 : sliceS.length - sliceS.lastIndexOf(s[i]));
     }
     return answer;
 }
