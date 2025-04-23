0b323332f943301e2afecda2702f2c969987e168
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-23 22:41:58 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,11 @@
 function solution(array, commands) {
-    let answer = [];
-    for(let i = 0 ; i < commands.length ; i++){
-        answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=> a-b)[commands[i][2]-1]);
+    var answer = [];
+    
+    for(let i = 0 ; i < commands.length; i++){
+        const first = commands[i][0] - 1;
+        const second = commands[i][1];
+        const third = commands[i][2] - 1;
+        answer.push(array.slice(first,second).sort((a,b) => a-b)[third]);
     }
     return answer;
 }
