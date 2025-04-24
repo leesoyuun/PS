2fb76f17dc631df78c138db53cf08ce2dffcd54b
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-24 23:51:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,7 +1,8 @@
 function solution(food) {
-    let first = '';
-    for(let i = 1 ; i <food.length ; i++){
-        first+=String(i).repeat(Math.floor(food[i]/2));
+    var answer = '';
+    
+    for(let i = 1 ; i < food.length ; i++){
+        answer += i.toString().repeat(Math.floor(food[i]/2));
     }
-    return first+'0'+[...first].reverse().join('');
+    return answer+'0'+[...answer].reverse().join('');
 }
