8df029a3b8c7dea504383ea646176d56966172e5
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-24 23:18:46 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,12 @@
 function solution(s) {
-    let en = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    let answer = s;
+    const alphaNum = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     
-    for(let i = 0 ; i < en.length ; i++){
-        let str = answer.split(en[i]);
+    var answer = s;
+    
+    for(let i = 0 ; i < alphaNum.length ; i++){
+        let str = answer.split(alphaNum[i]);
         answer = str.join(i);
     }
     return +answer;
+    
 }
