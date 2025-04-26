bc57e98161f51b880ea6a4ba3d544cb17dbb6e93
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-26 21:17:30 +0900

    [level 1] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 16:32:49
+2025년 04월 26일 21:17:28
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,10 +1,8 @@
 function solution(cards1, cards2, goal) {
     for(let i of goal){
-        if(cards1[0] === i){
-            cards1.splice(0,1);
-        }else if(cards2[0] === i){
-            cards2.splice(0,1);
-        }else return "No";
+        if(cards1[0] === i) cards1.splice(0,1);
+        else if(cards2[0] === i) cards2.splice(0,1);
+        else return "No";
     }
     return "Yes";
 }
