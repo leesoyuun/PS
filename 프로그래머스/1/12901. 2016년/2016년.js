6c3e75bcff2d5f55203e13eb752f8c5e8d859a9f
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-28 23:11:29 +0900

    [level 1] Title: 2016년, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,10 +1,13 @@
 function solution(a, b) {
-    let Week = ["THU","FRI","SAT",'SUN',"MON","TUE","WED"];
-    let Month = [31,29,31,30,31,30,31,31,30,31,30,31];
+    const DayOfWeek = ["THU","FRI","SAT","SUN","MON","TUE","WED"];
+    const Month = [31,29,31,30,31,30,31,31,30,31,30,31];
     let day = 0;
-    for(let i = 0 ; i < a-1 ; i++){
+    
+    for(let i = 0 ; i < a - 1 ; i++){
         day += Month[i];
     }
     day += b;
-    return Week[day%7];
+    return DayOfWeek[day%7];
+    
+    
 }
