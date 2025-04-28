1d24b6ffe6b0485a65b5711138cdc8bf76ae55dd
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-29 00:01:13 +0900

    [level 1] Title: 모의고사, Time: 33.20 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,17 +1,18 @@
 function solution(answers) {
-    let person1 = [1,2,3,4,5];
-    let person2 = [2,1,2,3,2,4,2,5];
-    let person3 = [3,3,1,1,2,2,4,4,5,5];
-    let result = [0,0,0];
-    for(let i = 0 ; i <answers.length; i++){
-        if(answers[i]===person1[i%person1.length]) result[0]++;
-        if(answers[i]===person2[i%person2.length]) result[1]++;
-        if(answers[i]===person3[i%person3.length]) result[2]++;
+    const person1 = [1,2,3,4,5];
+    const person2 = [2,1,2,3,2,4,2,5];
+    const person3 = [3,3,1,1,2,2,4,4,5,5];
+    
+    let answer = [0,0,0];
+    for(let i = 0 ; i < answers.length ; i++){
+        if(answers[i] === person1[i % person1.length]) answer[0]++;
+        if(answers[i] === person2[i % person2.length]) answer[1]++;
+        if(answers[i] === person3[i % person3.length]) answer[2]++;
     }
-    let big = Math.max(...result);
-    let answer = [];
-    for(let i = 0 ; i < result.length ; i++){
-        if(result[i] === big) answer.push(i+1);
+    let big = Math.max(...answer);
+    let total = [];
+    for(let i = 0 ; i < answer.length ; i++){
+        if(answer[i] === big) total.push(i+1);
     }
-    return answer.sort();
+    return total.sort();
 }
