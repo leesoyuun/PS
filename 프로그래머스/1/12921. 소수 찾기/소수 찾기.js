1ebbe32416118606e6803d6d90eafb0b9891185b
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-30 22:15:36 +0900

    [level 1] Title: 소수 찾기, Time: 54.05 ms, Memory: 44.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,14 +1,14 @@
 function solution(n) {
-    let arr = Array(n + 1).fill(true);
+    let arr = Array(n+1).fill(true);
     arr[0] = false;
     arr[1] = false;
-
-    for(let i = 2; i * i <= n; i++) {
-        if(arr[i]) {
-            for(let j = i * i; j <= n; j += i) {
+    
+    for(let i = 2 ; i * i <= n ; i++){
+        if(arr[i]){
+            for(let j = i*i; j <= n ; j+=i){
                 arr[j] = false;
             }
         }
     }
-    return arr.filter(el => el).length;
+    return arr.filter((val) => val).length;
 }
