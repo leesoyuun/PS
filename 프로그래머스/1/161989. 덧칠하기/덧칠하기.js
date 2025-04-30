f1c2f283d5e3e4cc8a99fb5e349a422239d95c3b
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-04-30 22:30:17 +0900

    [level 1] Title: 덧칠하기, Time: 40.60 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,6 +1,7 @@
 function solution(n, m, section) {
-    let painted = 0;
-    let answer = 0;
+    var answer = 0;
+    var painted = 0;
+    
     for(let x of section){
         if(painted < x){
             painted = x+m-1;
