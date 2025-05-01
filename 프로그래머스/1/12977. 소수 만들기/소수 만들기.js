e11b040e947feed0183eb8a07146c77a16537bee
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-05-02 00:18:01 +0900

    [level 1] Title: 소수 만들기, Time: 128.30 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,21 +1,20 @@
-function decimal(nums){
-    let total = 0;
-    for(let i = 2 ; i < nums; i++){
-        if(nums%i === 0) total++;
+function isDemical(num){
+    var cnt = 0;
+    for(let i = 2 ; i <= num ; i++){
+        if(num%i===0) cnt++;
     }
-    if(total === 0) return true;
-    else return false;
+    return cnt === 1;
 }
+
 function solution(nums) {
-    let answer = 0;
-    for(let i = 0 ; i < nums.length-2 ; i++){
-        for(let j = i+1 ; j < nums.length-1 ; j++){
-            for(let k = j+1 ; k < nums.length ; k++){
-                let sum = 0;
-                sum = nums[i] + nums[j] + nums[k];
-                decimal(sum) ? answer++ : 0;
+    var answer = 0;
+    for(let i = 0; i < nums.length - 2  ; i++){
+        for(let j = i+1 ; j < nums.length - 1 ; j++){
+            for(let k = j+1; k < nums.length ; k++){
+                if(isDemical(nums[i] + nums[j] + nums[k])) answer++;
             }
         }
     }
+    
     return answer;
 }
