174a51196ba1a1fdd210f0c07d8a51e6c5017a90
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-05-04 12:47:17 +0900

    [level 1] Title: 문자열 나누기, Time: 30.48 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,16 +1,16 @@
 function solution(s) {
-    let stack = [];
-    let answer = 0;
-    let first = s[0];
+    var stack = [];
+    var firstLetter = s[0];
+    var answer = 0;
     
     for(let i = 0 ; i < s.length ; i++){
-        if(first === s[i]) stack.push(s[i]);
-        if(first !== s[i]) stack.pop();
+        if(firstLetter === s[i]) stack.push(s[i]);
+        else stack.pop();
         
-        if(stack.length === 0){
+        if(stack.length === 0) {
             answer++;
-            first = s[i+1];
+            firstLetter = s[i+1];
         }
     }
-    return stack.length !==0 ? answer+1 : answer;
+    return stack.length !== 0 ? answer+1 : answer;
 }
