d47831f385359fc51007679b0e5dc3d11268f4f1
이소윤 <51051548+leesoyuun@users.noreply.github.com>
2025-05-06 21:34:03 +0900

    [level 1] Title: 대충 만든 자판, Time: 22.10 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,19 +1,21 @@
 function solution(keymap, targets) {
-    let answer = [];
-    targets.forEach((x)=>{
-        let press = 0;
-        for(let i = 0 ; i < x.length ; i++){
-            let cnt = Infinity;
-            keymap.forEach((k)=> {
-                let idx = k.indexOf(x[i]);
-                if(idx > -1){
-                    cnt = Math.min(cnt,idx+1);
-                }
-            });
-            press += cnt;
+    const answer = [];
+    
+    for(let i = 0 ; i < targets.length ; i++){
+        let targetsIndexSum = 0;
+        for(let j = 0 ; j < targets[i].length ; j++){
+            //여기까지 targets
+            let indexVal = Infinity;
+            for(let k = 0 ; k < keymap.length ; k++){
+                const targetsIndex = keymap[k].indexOf(targets[i][j]);
+                
+                 if(targetsIndex>-1){
+                     indexVal = Math.min(indexVal,targetsIndex+1);
+                 }
+            }
+            targetsIndexSum+=indexVal;
         }
-        answer.push(press);
-    })
-    answer = answer.map(val => val === Infinity ? -1 : val);
+        answer.push(targetsIndexSum === Infinity ? -1 : targetsIndexSum);
+    }
     return answer;
 }
